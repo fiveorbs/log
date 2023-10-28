a1617a0785b826249cb2ef6892ac3cd6733e8f4c
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-10-28 10:20:12 +0200

    Revise base TestCase: add factory and error_reporting

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace Conia\Error\Tests;
 
+use Nyholm\Psr7\Factory\Psr17Factory;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
+    protected ?int $defaultErrorReporting;
     protected mixed $defaultLog;
     protected mixed $tempFile;
     protected mixed $logFile;
+    protected ?Psr17Factory $factory;
 
     public function setUp(): void
     {
@@ -20,6 +23,11 @@ class TestCase extends BaseTestCase
         $this->tempFile = tmpfile();
         $this->logFile = stream_get_meta_data($this->tempFile)['uri'];
         ini_set('error_log', $this->logFile);
+
+        $this->defaultErrorReporting = error_reporting();
+        error_reporting(E_ALL);
+
+        $this->factory = new Psr17Factory();
     }
 
     public function tearDown(): void
@@ -29,6 +37,14 @@ class TestCase extends BaseTestCase
         ini_set('error_log', $this->defaultLog);
         restore_error_handler();
         restore_exception_handler();
+        error_reporting($this->defaultErrorReporting);
+
+        $this->defaultErrorReporting = null;
+        $this->defaultLog = null;
+        $this->tempFile = null;
+        $this->logFile = null;
+
+        $this->factory = null;
     }
 
     public function throws(string $exception, string $message = null): void
